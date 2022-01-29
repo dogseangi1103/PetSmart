94392e4a7635a99dd9f2b36653a4c4e71c2859c2
Dog <dogseango1103@gmail.com>
2022-01-29 23:01:05 +0800

    [取得商品] TestCase 取得商品需與DB商品數量相同

--- Business.UnitTests/Servcies/ProductServiceTests.cs
+++ Business.UnitTests/Servcies/ProductServiceTests.cs
@@ -1,18 +1,40 @@
-﻿using NUnit.Framework;
+﻿using Business.Services;
+using Data.Context;
+using Data.Entities;
+using Moq;
+using Moq.EntityFrameworkCore;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Business.UnitTests.Servcies
 {
     public class ProductServiceTests
     {
+        private Mock<DogseandatabaseContext> _dbContext;
+        private IProductService _productService;
+
         [SetUp]
         public void Setup()
         {
+            _dbContext = new Mock<DogseandatabaseContext>();
+            _productService = new ProductService(_dbContext.Object);
         }
 
         [Test]
-        public void Test1()
+        public void GetProducts_WhenCalled_ReturnSameProductCountInDb()
         {
-            Assert.Pass();
+            var products = new List<Product>
+            {
+                new Product { Id = 1, Name = "a1" },
+                new Product { Id = 2, Name = "a2" },
+                new Product { Id = 3, Name = "a3" },
+            };
+            _dbContext.Setup(ps => ps.Product).ReturnsDbSet(products);
+
+            var result = _productService.GetProducts();
+
+            Assert.That(result.Count, Is.EqualTo(products.Count));
         }
     }
 }

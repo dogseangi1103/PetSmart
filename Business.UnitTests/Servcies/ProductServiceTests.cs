8bd3236651296839da6c9ece16128534a0e4e705
Dog <dogseango1103@gmail.com>
2022-01-29 22:21:29 +0800

    [取得商品] 新增UnitTest專案

---
+++ Business.UnitTests/Servcies/ProductServiceTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace Business.UnitTests.Servcies
+{
+    public class ProductServiceTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}

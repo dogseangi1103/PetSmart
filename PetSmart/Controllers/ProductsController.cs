fc3bfd8cbc5391c1f1c6a5510ff19c883dc5fa91
Dog <dogseango1103@gmail.com>
2022-01-29 20:29:02 +0800

    [取得商品] 取得商品API

---
+++ PetSmart/Controllers/ProductsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PetSmart.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet(Name = "GetProducts")]
+        public IActionResult Get()
+        {
+            var products = new List<dynamic>
+            {
+                new { Id = 1, Name = "dog food 1" },
+                new { Id = 2, Name = "dog food 2" },
+                new { Id = 3, Name = "dog food 3" },
+            };
+            return Ok(products);
+        }
+    }
+}

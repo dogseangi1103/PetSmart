522186db06dc19f3c276fbcd5b507122fe8cd3b5
Dog <dogseango1103@gmail.com>
2022-01-30 10:17:23 +0800

    [訂單付款] TestCase 訂單未付款時 進行付款 則該訂單轉為已付款

---
+++ Business.UnitTests/Servcies/OrderServiceTests.cs
@@ -0,0 +1,45 @@
+﻿using Business.Services;
+using Data.Context;
+using Data.Entities;
+using Moq;
+using Moq.EntityFrameworkCore;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Business.UnitTests.Servcies
+{
+    public class OrderServiceTests
+    {
+        private Mock<DogseandatabaseContext> _dbContext;
+        private IOrderService _orderService;
+
+        public Order unpaidOrder { get; set; }
+        public Order paidOrder { get; set; }
+        public Order deliveredOrder { get; set; }
+
+        [SetUp]
+        public void Setup()
+        {
+            _dbContext = new Mock<DogseandatabaseContext>();
+            _orderService = new OrderService(_dbContext.Object);
+
+            // Todo: Status Enum
+            unpaidOrder = new Order { Id = 11, Address = "a1", Status = 1 };
+            paidOrder = new Order { Id = 12, Address = "a2", Status = 2 };
+            deliveredOrder = new Order { Id = 13, Address = "a3", Status = 3 };
+            var orders = new List<Order> { unpaidOrder, paidOrder, deliveredOrder };
+            _dbContext.Setup(ps => ps.Order).ReturnsDbSet(orders);
+        }
+
+        [Test]
+        public async Task Pay_OrderIsUnpaid_OrderStatusChangeToPaid()
+        {
+            var result = await _orderService.Pay(unpaidOrder.Id);
+
+            // Todo: Status Enum
+            Assert.That(result.Status == 2);
+        }
+    }
+}
---
+++ Business/Services/OrderService.cs
@@ -0,0 +1,26 @@
+﻿using Data.Context;
+using Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Services
+{
+    public class OrderService : IOrderService
+    {
+        private readonly DogseandatabaseContext _dbContext;
+
+        public OrderService(DogseandatabaseContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public Task<Order> Pay(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
